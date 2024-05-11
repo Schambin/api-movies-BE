ed27c987e84e77be31524645fe64ae9445ea5d0d
Kauan <raidoka546@gmail.com>
2024-05-11 10:51:24 -0400

    configurated express async errors to catch client and server side errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,13 @@
 //All the user actions like get, post, put n delete.
+const AppError = require('../utils/AppError');
+
 class UsersController {
     create(req, res) {
         const { username, email, password } = req.body;
+
+        if (!username) {
+            throw new AppError('Invalid Username')
+        }
 
         res.send(`${username} \n ${email} \n ${password}`);
     }
