f84f4406cd35d62e81986b2584952ea338f9174d
Kauan <raidoka546@gmail.com>
2024-05-16 15:35:00 -0400

    removed middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,15 +5,20 @@ const usersRoutes = Router();
 
 const UsersController = require('../controllers/UsersController')
 
-checkIfIsAdmin = (req, res, next) => {
-    if (req.body.isAdmin === true) {
-        return next();
-    }
-    return res.status(401).json({ error: 'Usuário Não Autorizado' });
-}
+/*
+ * MiddleWare Check
+ *
+ *  checkIfIsAdmin = (req, res, next) => {
+ *   if (req.body.isAdmin === true) {
+ *       return next();
+ *   }
+ *      return res.status(401).json({ error: 'Usuário Não Autorizado' });
+ *   }
+ */
 
 const usersController = new UsersController();
 
-usersRoutes.post('/', checkIfIsAdmin, usersController.create);
+usersRoutes.post('/', usersController.create);
+usersRoutes.put('/:id', usersController.update);
 
 module.exports = usersRoutes;
