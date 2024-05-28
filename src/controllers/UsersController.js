ba1c87b3c9089a688ffe48b3503cb942a5d7d440
Kauan <raidoka546@gmail.com>
2024-05-28 09:44:34 -0400

    literally a .

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -5,6 +5,7 @@ const AppError = require('../utils/AppError');
 const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
+
     async create(req, res) {
         const { name, email, password } = req.body;
 
@@ -20,7 +21,7 @@ class UsersController {
             throw new AppError('Este email já está em uso')
         }
 
-        const hashedPassword = await hash(password, 8);
+        const hashedPassword = await bcrypt.hash(password, 8);
 
         await database.run(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [name, email, hashedPassword]);
 
